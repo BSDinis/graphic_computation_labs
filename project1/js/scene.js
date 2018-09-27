7b844281c80b666543986139b148f67e0ebf40cd
BSDinis <baltasardinis@hotmail.com>
2018-09-27 02:47:24 +0100

    added skeleton scene.js

---
+++ project1/js/scene.js
@@ -0,0 +1,29 @@
+/**
+ * scene.js
+ *
+ * define the scene class
+ *   - lamp
+ *   - table
+ *   - chair
+ */
+
+// position of the center of the table
+const tableX = 0;
+const tableY = 0;
+
+// position of the center of the lamp
+const lampX = 0;
+const lampY = 0;
+
+// position of the center of the chair
+const chairX = 0;
+const chairY = 0;
+
+
+class Scene {
+  constructor() {
+    this.lamp = new Lamp(lampX, lampY);
+    this.table = new Table(tableX, tableY);
+    this.chair = new Chair(chairX, chairY);
+  }
+}
