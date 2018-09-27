fb04bef660dc7d7e16efd79bdb1f1eb97adc6547
BSDinis <baltasardinis@hotmail.com>
2018-09-27 02:55:27 +0100

    added table skeleton

---
+++ project1/js/table.js
@@ -0,0 +1,14 @@
+/*
+ * table.js
+ *
+ * define table class
+ */
+
+class Table {
+  constructor(tableX, tableY, colour) {
+    this.X = tableX;
+    this.Y = tableY;
+    this.Z = tableZ;
+    this.colour = colour;
+  }
+}
