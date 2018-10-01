a5033974c3f67388ff68a2d268884e7b500d26ac
BSDinis <baltasardinis@hotmail.com>
2018-10-01 21:19:11 +0100

    classed everything up, very pretty

--- project1/js/chair.js
+++ project1/js/chair.js
@@ -6,7 +6,7 @@
 
 class Chair {
   constructor(position, dimensions, inputColor, scene) {
-    this._chair = new THREE.Object3D();
+    this.chair = new THREE.Object3D();
     this._speed = 0;
     this._acceleration = 0;
     this._angularSpeed = 0;
@@ -14,112 +14,19 @@ class Chair {
 
     this._material = new THREE.MeshBasicMaterial({color: inputColor, wireframe: true});
 
-    this._rod = constructRod(
+    this.rod = new Rod(
       {x: 0,
        y: 2*dimensions.wheelRadius + dimensions.rodHeight / 2,
        z: 0}, 
       dimensions,
-      this._material
+      this._material,
+      this.chair
     );
 
-    this._chair.add(this._rod);
-    this._chair.position.set(position.x, position.y, position.z);
-    scene.add(this._chair);
-    return;
-
-
-    function constructRod(position, dimensions, material) {
-      var geometry = new THREE.CubeGeometry(dimensions.rodRadius * 2,
-          dimensions.rodHeight,
-          dimensions.rodRadius * 2);
-
-      var rod = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial(material));
-
-      var seat = constructSeat(
-        {x: 0, y: dimensions.rodHeight/2 + dimensions.seatThickness / 2, z: 0},
-        dimensions,
-        material
-      );
-
-      rod.add(seat);
-
-      for (var i = 0; i < dimensions.noLegs; i++) {
-        var leg = constructLeg(
-          {x: 0, y: -dimensions.rodHeight/2 - dimensions.legRadius, z:0},
-          dimensions,
-          material,
-          i
-        );
-        rod.add(leg);
-      }
-
-      rod.position.x = position.x;
-      rod.position.y = position.y;
-      rod.position.z = position.z;
-      return rod;
-    }
-
-    function constructSeat(position, dimensions, material) {
-	  var geometry = new THREE.CubeGeometry(dimensions.seatRadius * 2,
-        dimensions.seatThickness,
-        dimensions.seatRadius * 2);
-
-      var seat = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial(material));
-      var back = constructBack(
-        {x: 0, y: dimensions.backHeight / 2 - dimensions.seatThickness/2, z: dimensions.seatRadius + dimensions.backThickness / 2},
-        dimensions,
-        material
-      );
-      seat.add(back);
-
-      seat.position.x = position.x;
-      seat.position.y = position.y;
-      seat.position.z = position.z;
-      return seat;
-    }
-
-    function constructBack(position, dimensions, material) {
-	  var geometry = new THREE.CubeGeometry(dimensions.backWidth,
-        dimensions.backHeight,
-        dimensions.backThickness);
-
-      var back = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial(material));
-      back.position.x = position.x;
-      back.position.y = position.y;
-      back.position.z = position.z;
-      return back;
-    }
-
-    function constructLeg(position, dimensions, material, legNo) {
-      var geometry = new THREE.CubeGeometry(2*dimensions.legRadius, 2*dimensions.legRadius, dimensions.legLength);
-      var leg = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial(material));
-
-      var wheel = new constructWheel(
-        {x: 0, y: -dimensions.wheelRadius, z: -dimensions.legLength/2},
-        dimensions,
-        material
-      );
-
-      leg.add(wheel);
-
-      var angle = 2 * legNo * Math.PI / dimensions.noLegs;
-      leg.position.x = 0;
-      leg.position.y = position.y;
-      leg.position.z = position.z - dimensions.legLength / 2
-      leg.position.applyAxisAngle(new THREE.Vector3(0, 1, 0), angle);
-      leg.rotateY(angle);
-
-      return leg;
-    }
-
-    function constructWheel(position, dimensions, material) {
-      var geometry = new THREE.TorusGeometry(dimensions.wheelRadius, dimensions.wheelRadius, 16, 16);
-      var wheel = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial(material));
-
-      wheel.position.set(position.x, position.y, position.z);
-      return wheel;
-    }
+    this.chair.position.set(position.x, position.y, position.z);
+    scene.add(this.chair);
   }
+
 
   hasAcceleration(){
     return this._acceleration != 0 || this._angularAcceleration != 0;
@@ -150,11 +57,11 @@ class Chair {
   }
 
   updatePosition(delta){
-    this._chair.position.z += delta * this._speed;
+    this.chair.position.z += delta * this._speed;
   }
 
   updateRotation(delta){
-    this._chair.rotateY(delta * this._angularSpeed);
+    this.rod.updateRotation(delta * this._angularSpeed);
   }
 
   setAcceleration(a){
@@ -173,5 +80,114 @@ class Chair {
   getFriction(){
     const factor = 2;
     return - this._speed * factor;
+  }
+}
+
+
+class Rod {
+  constructor(position, dimensions, material, parentObj) {
+    var geometry = new THREE.CubeGeometry(dimensions.rodRadius * 2,
+        dimensions.rodHeight,
+        dimensions.rodRadius * 2);
+
+    this.mesh = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial(material));
+    this.seat = new Seat(
+      {x: 0, y: dimensions.rodHeight/2 + dimensions.seatThickness / 2, z: 0},
+      dimensions,
+      material,
+      this.mesh
+    );
+
+    var angle = 0;
+    var angleStep = 2 * Math.PI / 5;
+    this.leg = []
+    for (var i = 0; i < dimensions.noLegs; i++, angle += angleStep) {
+      this.leg[i] = new Leg(
+        {x: 0, y: -dimensions.rodHeight/2 - dimensions.legRadius, z:0},
+        dimensions,
+        material,
+        angle,
+        this.mesh
+      );
+    }
+
+    this.mesh.position.x = position.x;
+    this.mesh.position.y = position.y;
+    this.mesh.position.z = position.z;
+    parentObj.add(this.mesh);
+
+  }
+
+  updateRotation(angle) {
+    this.seat.updateRotation(angle);
+  }
+}
+
+class Leg {
+  constructor(position, dimensions, material, angle, parentObj) {
+    var geometry = new THREE.CubeGeometry(2*dimensions.legRadius, 2*dimensions.legRadius, dimensions.legLength);
+    this.mesh = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial(material));
+
+    var wheel = new Wheel(
+      {x: 0, y: -dimensions.wheelRadius, z: -dimensions.legLength/2},
+      dimensions,
+      material,
+      this.mesh
+    );
+
+    this.mesh.position.x = 0;
+    this.mesh.position.y = position.y;
+    this.mesh.position.z = position.z - dimensions.legLength / 2
+    this.mesh.position.applyAxisAngle(new THREE.Vector3(0, 1, 0), angle);
+    this.mesh.rotateY(angle);
+
+    parentObj.add(this.mesh);
+  }
+}
+
+class Wheel {
+  constructor(position, dimensions, material, parentObj) {
+    var geometry = new THREE.TorusGeometry(dimensions.wheelRadius, dimensions.wheelRadius, 16, 16);
+    this.mesh = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial(material));
+
+    this.mesh.position.set(position.x, position.y, position.z);
+    this.mesh.rotateY(Math.PI / 2);
+    parentObj.add(this.mesh);
+  }
+}
+
+class Seat {
+  constructor(position, dimensions, material, parentObj) {
+    var geometry = new THREE.CubeGeometry(dimensions.seatRadius * 2,
+      dimensions.seatThickness,
+      dimensions.seatRadius * 2);
+
+    this.mesh = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial(material));
+    this.back = new Back(
+      {x: 0, y: dimensions.backHeight / 2 - dimensions.seatThickness/2, z: dimensions.seatRadius + dimensions.backThickness / 2},
+      dimensions,
+      material,
+      this.mesh
+    );
+
+    this.mesh.position.set(position.x, position.y, position.z);
+    parentObj.add(this.mesh);
+
+  }
+
+  updateRotation(angle) {
+    this.mesh.rotateY(angle);
+  }
+}
+
+class Back {
+  constructor(position, dimensions, material, parentObj) {
+    var geometry = new THREE.CubeGeometry(dimensions.backWidth,
+      dimensions.backHeight,
+      dimensions.backThickness);
+
+    this.mesh = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial(material));
+    this.mesh.position.set(position.x, position.y, position.z);
+    parentObj.add(this.mesh);
   }
 }
