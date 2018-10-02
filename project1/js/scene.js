5523743dfac309a152fd4a4d3194f6972ecbc237
BSDinis <baltasardinis@hotmail.com>
2018-10-02 01:12:08 +0100

    resized and coloured rug

--- project1/js/scene.js
+++ project1/js/scene.js
@@ -54,7 +54,7 @@ class Scene {
     axis.visible = false;
     this.scene.add(axis);
 
-    this.rug = new Rug(1000, 1000, 5, this.scene);
+    this.rug = new Rug(800, 500, 5, this.scene);
     // private
     this.chair = new Chair({x: 0, y: 0, z: 100}, chairDimensions, 0xff0000, this.rug.obj);
     this.table = new Table({x: 0, y: 0, z: 0}, tableDimensions, 0x00ff00, this.rug.obj);
@@ -78,7 +78,7 @@ class Scene {
 class Rug {
   constructor(width, height, thickness, parentObj) {
     this.obj = new THREE.Object3D();
-    var material = new THREE.MeshBasicMaterial({color: 0xffffff, wireframe: true});
+    var material = new THREE.MeshBasicMaterial({color: 0xbbbb00, wireframe: true});
     var geometry = new THREE.CubeGeometry(width, thickness, height);
     this.mesh = new THREE.Mesh(geometry, material);
     this.mesh.position.y = -thickness/2;
