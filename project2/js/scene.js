8a355474f0ed7b70ac627e01ee05ab516e0102a7
BSDinis <baltasardinis@hotmail.com>
2018-10-22 12:22:28 +0100

    created random colour generator, position generator (that avoids conflicts) and composed the scene

--- project2/js/scene.js
+++ project2/js/scene.js
@@ -4,18 +4,27 @@
  * define the scene class
  */
 
+const nBalls = 10;
+
 class Scene {
   constructor() {
     // public
     this.scene = new THREE.Scene();
 
     this.ring = new Ring(100, 0xff0000, this.scene);
-    /*
+
+    var innerWidth = this.ring.getWidth() - this.ring.getDepth() * 1.1
+    var innerHeight = this.ring.getHeight() - this.ring.getDepth() * 1.1
+    var radius = this.ring.getDepth()/2
+    var initialPosition = genInitialPositions(innerWidth, innerHeight, radius, nBalls);
+
     this.ballArr = [];    
-    for (var i = 0; i < 10; i++) {
-      this.ballArr[i] = new Ball(this.ring.getDepth()/2, 0xff00000, this.scene);
+    for (var i = 0; i < nBalls; i++) {
+      this.ballArr[i] = new Ball(radius, genRandomColor(), this.scene);
+      this.ballArr[i].obj.position.x = initialPosition[i].x
+      this.ballArr[i].obj.position.z = initialPosition[i].z
+      this.ballArr[i].obj.position.y += radius;
     }
-    */
   }
 
   getAspect() {
@@ -37,4 +46,43 @@ class Scene {
   updateScene(delta) {
     // FIXME
   }
+}
+
+
+function genInitialPositions(w, h, radius, n)
+{
+
+  function conflict(list, len, new_el, r) {
+    function el_conflict(el1, el2, r) {
+      var w = el1.x - el2.x;
+      var h = el1.z - el2.z;
+      return w * w + h * h <= radius * radius * 4
+    }
+    for (var j = 0; j < len; j++) {
+      if (el_conflict(list[j], new_el, r)) return true
+    }
+
+    return false
+  }
+  pos = [];
+  for (var i = 0; i < n; i++) {
+    var _x;
+    var _z;
+    var p;
+    do {
+      _x = Math.floor(Math.random() * w) - w/2;
+      _z = Math.floor(Math.random() * h) - h/2;
+      p = {x: _x, z: _z};
+    } while (conflict(pos, i, p, radius));
+    pos[i] = p;
+  }
+  return pos;
+}
+
+function genRandomColor()
+{
+  var r = Math.floor(Math.random() * 200) + 56;
+  var g = Math.floor(Math.random() * 200) + 56;
+  var b = Math.floor(Math.random() * 200) + 56;
+  return (r * 256 + g) * 256 + b;
 }
