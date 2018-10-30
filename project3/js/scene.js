27dfdf5392f4f736a83b91184ebc3fd7c983e670
BSDinis <baltasardinis@hotmail.com>
2018-10-30 09:18:29 +0000

    added base

--- project3/js/scene.js
+++ project3/js/scene.js
@@ -4,12 +4,19 @@
  * define the scene class
  */
 
+const object_factor = 200
+const base_factor =  300
+
 class Scene {
   constructor() {
     this.scene = new THREE.Scene();
-    this.example = new Example(100, 0x33ff22, this.scene);
+    this.example = new Example(object_factor, 0x33ff22, this.scene);
+    this.example.obj.position.y += this.example.getDepth() / 2
+    this.base = new Base(base_factor, 0xff6666, this.scene);
     var dim = {width: this.getWidth(), height: this.getHeight(), depth: this.getDepth()};
     this.sunlight = new SunLight(this.scene, dim);
+    this.ambientlight = new THREE.AmbientLight(0xffffff)
+    this.scene.add(this.ambientlight)
   }
 
   getAspect() {
@@ -17,17 +24,14 @@ class Scene {
   }
 
   getWidth() {
-    // FIXME
-    return this.example.getWidth() * 1.15;
+    return this.base.getWidth() * 1.15;
   }
 
   getHeight() {
-    // FIXME
-    return this.example.getHeight() * 1.15;
+    return this.base.getHeight() * 1.15;
   }
 
   getDepth() {
-    // FIXME
     return this.example.getDepth() * 1.15;
   }
 
--- project3/js/sunlight.js
+++ project3/js/sunlight.js
@@ -8,8 +8,8 @@ const intensity = 1.0
 class SunLight {
   constructor(parentObj, dim) {
     this.light = new THREE.DirectionalLight(colour, intensity);
-	this.light.position.set( dim.height, dim.width, dim.depth );
-	this.light.castShadow = true;
+	this.light.position.set( dim.width, dim.depth, dim.height );
+    //this.light.castShadow = true;
     parentObj.add(this.light);
   }
 }
