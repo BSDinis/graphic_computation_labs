5355c4e9b3ac82311a739bfbbb9939bda1277de2
BSDinis <baltasardinis@hotmail.com>
2018-10-30 09:19:59 +0000

    made sunlight cast rays

--- project3/js/sunlight.js
+++ project3/js/sunlight.js
@@ -9,7 +9,7 @@ class SunLight {
   constructor(parentObj, dim) {
     this.light = new THREE.DirectionalLight(colour, intensity);
 	this.light.position.set( dim.width, dim.depth, dim.height );
-    //this.light.castShadow = true;
+    this.light.castShadow = true;
     parentObj.add(this.light);
   }
 }
