d49058fee495760debf5fc67f55d534bcc1c8e56
BSDinis <baltasardinis@hotmail.com>
2018-11-05 03:28:58 +0000

    adjusted position

--- project3/js/sunlight.js
+++ project3/js/sunlight.js
@@ -8,7 +8,7 @@ const intensity = 1.0
 class SunLight {
   constructor(parentObj, dim) {
     this.light = new THREE.DirectionalLight(colour, intensity);
-	this.light.position.set( dim.width, dim.depth, dim.height );
+	this.light.position.set(10* dim.width, 55*dim.depth, 10* dim.height );
     this.light.castShadow = true;
     parentObj.add(this.light);
   }
