b44e9231c262a0d1779531dc7513213b2a85b92c
BSDinis <baltasardinis@hotmail.com>
2018-11-17 21:58:12 +0000

    simplified materials

--- project4/js/materials.js
+++ project4/js/materials.js
@@ -1,12 +1,10 @@
 /**
- *
  * define wrapper do define several materials at once
  */
 
 function genMaterials(inputColor, _wireframe) {
   var materials = []
   materials.push(new THREE.MeshBasicMaterial({color: inputColor, wireframe: _wireframe}));
-  materials.push(new THREE.MeshLambertMaterial({color: inputColor, wireframe: _wireframe}));
-  materials.push(new THREE.MeshPhongMaterial({color: inputColor, wireframe: _wireframe}));
+  materials.push(new THREE.MeshStandardMaterial({color: inputColor, wireframe: _wireframe}));
   return materials
  }
